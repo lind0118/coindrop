7c9d208aa068ca4209ba79451e1cc3b7543b11c1
satokihanada <satoki0212@gmail.com>
2024-09-19 15:15:45 +0900

    piggybank context + dashboard updates (#5)
    * wip [skip ci]
    
    * cleanup
    
    * rename logo folder
    
    * rename logo folder
    
    * wip [skip ci]
    
    * wip [skip ci]
    
    * wip [skip ci]
    
    * wip [skip ci]
    
    * wip [skip ci]
    
    * update readme
    
    * wip [skip ci]
    
    * wip [skip ci]
    
    * wip [skip ci]
    
    * wip [skip ci]
    
    * wip [skip ci]
    
    * wip [skip ci]
    
    * wip [skip ci]
    
    * wip [skip ci]
    
    * wip [skip ci]
    
    * wip [skip ci]
    
    * wip [skip ci]
    
    * wip [skip ci]
    
    * wip [skip ci]
    
    * wip [skip ci]
    
    * wip [skip ci]
    
    * wip [skip ci]
    
    * wip [skip ci]
    
    * add theme colors, next up: edit page
    
    * wip [skip ci]
    
    * wip [skip ci]
    
    * wip [skip ci]
    
    * edit modal updates
    
    Co-authored-by: lind0118 <lind.daniel0118@gmail.com>
    Co-authored-by: satokihanada <satoki0212@gmail.com>

---
+++ components/Dashboard/UserOwnedPiggybanks/UserOwnedPiggybanks.jsx
@@ -0,0 +1,71 @@
+import PropTypes from 'prop-types';
+import useSWR from 'swr';
+import { Box, Flex, Heading, Spinner, Stack } from '@chakra-ui/core';
+import { db } from '../../../utils/client/db';
+import PiggybankListItem from './PiggybankListItem';
+import AddPiggybankListItem from './AddPiggybankListItem/AddPiggybankListItem';
+
+async function fetchUserOwnedPiggybanks(uid) {
+    const piggybanks = await db
+        .collection('piggybanks')
+        .where('owner_uid', '==', uid)
+        .get();
+    if (piggybanks.empty) {
+        console.log('No matching documents.');
+        return [];
+    }
+    const piggybankData = [];
+    piggybanks.forEach(piggybank => {
+        piggybankData.push({
+            piggybankName: piggybank.id,
+        });
+    });
+    return piggybankData;
+}
+
+const UserOwnedPiggybanks = (props) => {
+    const { uid } = props;
+    const { data, error } = useSWR(uid, fetchUserOwnedPiggybanks);
+    const PiggybankContent = () => {
+        if (error) {
+            console.error(error);
+            return `Error getting piggybank data, please try refreshing the page.`;
+        }
+        if (data) {
+            const numActivePiggybanks = data.length;
+            return (
+                <Stack spacing={8} my={4}>
+                    {data.map(piggybank => (
+                        <PiggybankListItem
+                            key={piggybank.piggybankName}
+                            name={piggybank.piggybankName}
+                            uid={uid}
+                        />
+                    ))}
+                    <AddPiggybankListItem
+                        numActivePiggybanks={numActivePiggybanks}
+                    />
+                </Stack>
+            );
+        }
+        return <Spinner size="lg" />;
+    };
+    return (
+        <Box>
+            <Heading textAlign="center">
+                My Piggybanks
+            </Heading>
+            <PiggybankContent />
+        </Box>
+    );
+};
+
+UserOwnedPiggybanks.propTypes = {
+    uid: PropTypes.string.isRequired,
+};
+
+UserOwnedPiggybanks.defaultProps = {
+
+};
+
+export default UserOwnedPiggybanks;
